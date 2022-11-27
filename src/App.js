c340f05a83871a1be3df09b6a3e2e08fe9ffe91b
Abby  Kolzow <abbykolzow@Abbys-MacBook-Air.local>
2022-11-27 15:50:17 -0600

    feat: boy and girl lists, added hover effects on nav

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { MdCancel } from 'react-icons/md'
 import { CiAirportSign1 } from 'react-icons/ci'
 import { AiOutlineCheckCircle } from 'react-icons/ai'
 import { TbGenderDemigirl, TbGenderMale } from 'react-icons/tb'
+import { SlLike, SlDislike } from 'react-icons/sl'
 
 
 // Be able to find partners
@@ -24,17 +25,36 @@ function App() {
   const [nameIndex, setNameIndex] = useState(0)
   const [listKey, setListKey] = useState('boys')
   const [navState, setNavState] = useState('Home')
-  const [likedNames, setLikedNames] = useState([])
-  const [disLikedNames, setDisLikedNames] = useState([])
-  const handleclick = (event) => {
-    if (event === 'liked') {
-      setLikedNames(current => [...current, names[listKey][nameIndex]])
+  const [likedBoyNames, setLikedBoyNames] = useState([])
+  const [likedGirlNames, setLikedGirlNames] = useState([])
+
+  const [disLikedBoyNames, setDisLikedBoyNames] = useState([])
+  const [disLikedGirlNames, setDisLikedGirlNames] = useState([])
+
+
+
+  const handleclick = (event, listKey) => {
+
+
+
+    console.log(listKey)
+    if (event === 'liked' && listKey === 'boys') {
+      setLikedBoyNames(current => [...current, names[listKey][nameIndex]])
       setNameIndex(nameIndex + 1)
     }
-    if (event === 'disliked') {
-      setDisLikedNames(current => [...current, names[listKey][nameIndex]])
+    if (event === 'liked' && listKey === 'girls') {
+      setLikedGirlNames(current => [...current, names[listKey][nameIndex]])
       setNameIndex(nameIndex + 1)
     }
+    if (event === 'disliked' && listKey === 'boys') {
+      setDisLikedBoyNames(current => [...current, names[listKey][nameIndex]])
+      setNameIndex(nameIndex + 1)
+    }
+    if (event === 'disliked' && listKey === 'girls') {
+      setDisLikedGirlNames(current => [...current, names[listKey][nameIndex]])
+      setNameIndex(nameIndex + 1)
+    }
+
 
     // update liked or disliked
     // increate name counter
@@ -46,11 +66,18 @@ function App() {
   return (
     <div className="flex h-full w-full items-center flex-col">
       <nav className='flex items-center justify-center'>
-        <div onClick={() => setNavState('Home')} className='flex items-center mr-2 p-2  md:mr-4 md:p-4 cursor-pointer'> <FaBabyCarriage /> <button className='ml-2'>Home</button> </div>
-        <div onClick={() => setNavState('Partner')} className='flex items-center m-2 p-2 md:mr-4 md:p-4 cursor-pointer'><BsFillPersonFill /><button className='ml-2'>Partners</button></div>
-        <div onClick={() => setNavState('Matches')} className='flex items-center m-2 p-2 md:mr-4 md:p-4  cursor-pointer'><RiCheckboxMultipleLine /><button className='ml-2'>Matches</button></div>
+        <div onClick={() => setNavState('Home')} className={navState === 'Home' ?
+          'flex items-center mr-2 p-2  md:mr-4 md:p-4 cursor-pointer border-b-2 border-blue-500'
+          : 'flex items-center mr-2 p-2  md:mr-4 md:p-4 cursor-pointer border-b-2 border-transparent rounded-lg hover:bg-gray-50'}>
+          <FaBabyCarriage /> <button className='ml-2'>Home</button> </div>
+        <div onClick={() => setNavState('Partner')} className={navState === 'Partner' ?
+          'flex items-center mr-2 p-2  md:mr-4 md:p-4 cursor-pointer border-b-2 border-blue-500'
+          : 'flex items-center mr-2 p-2  md:mr-4 md:p-4 cursor-pointer border-b-2 border-transparent rounded-lg hover:bg-gray-50 '}><BsFillPersonFill /><button className='ml-2'>Partners</button></div>
+        <div onClick={() => setNavState('Matches')} className={navState === 'Matches' ?
+          'flex items-center mr-2 p-2  md:mr-4 md:p-4 cursor-pointer border-b-2 border-blue-500'
+          : 'flex items-center mr-2 p-2  md:mr-4 md:p-4 cursor-pointer border-b-2 border-transparent rounded-lg hover:bg-gray-50'}><RiCheckboxMultipleLine /><button className='ml-2'>Matches</button></div>
 
-      </nav>
+      </nav >
 
       {navState === 'Home' ?
 
@@ -66,41 +93,41 @@ function App() {
               <TbGenderDemigirl /></button>
 
           </div>
-          <div onClick={() => handleclick('disliked')} className='w-1/6 md:1/12 items-center justify-center flex flex-col cursor-pointer hover:bg-red-100 rounded-lg'><FcCancel /></div>
+          <div onClick={() => handleclick('disliked', listKey)} className='w-1/6 md:1/12 items-center justify-center flex flex-col cursor-pointer hover:bg-red-100 rounded-lg'><FcCancel /></div>
           <div className='w-2/3 md:10/12 justify-center items-center flex flex-col'>
             <h1 className='-mt-4 text-sm'>Name</h1>
             <h3 className='text-5xl'>{names[listKey][nameIndex]}</h3>
           </div>
-          <div onClick={() => handleclick('liked')} className='w-1/6 md:1/12 items-center justify-center   rounded-lg flex flex-col cursor-pointer hover:bg-green-100'><FcCheckmark /></div>
+          <div onClick={() => handleclick('liked', listKey)} className='w-1/6 md:1/12 items-center justify-center   rounded-lg flex flex-col cursor-pointer hover:bg-green-100'><FcCheckmark /></div>
 
         </div>
         : navState === 'Partner' ?
           // partner container
-          <div className="mt-12 min-h-1/4 w-3/4 md:w-1/2 rounded-lg xl:w-1/2 flex shadow-xl  justify-start items-center flex-col">
+          <div className="mt-4 min-h-1/4 w-3/4 md:w-1/2 rounded-lg xl:w-1/2 flex shadow-xl  justify-start items-center flex-col">
             <div className='border-b-2 p-4 w-full' >
               <div className='flex flex-col jusify-center items-center m-auto' >
                 <h4>send an invite</h4>
-                <div className='flex flex-row w-3/4 items-center'>
+                <div className='flex flex-row md:flex-row w-full sm:w-3/4 items-center'>
                   <BsSearch className='mr-2' />
                   <input placeholder='enter an email' className='flex-grow shadow p-4 outline-none rounded-lg' type='email'></input>
-                  <button type='submit' className='ml-2 p-4 rounded-lg shadow cursor-pointer'>send</button>
+                  <button type='submit' className='ml-2 p-2 pt-4 pb-4 md:p-4 rounded-lg shadow cursor-pointer'>send</button>
                 </div>
               </div>
             </div>
             <div className='w-full' >
               <div className='flex flex-col items-center'>
                 <h4>connected partners</h4>
-                <div className='w-3/4 m-2 p-4 shadow rounded-lg flex items-center'>
+                <div className='w-full md:w-1/2 m-2 p-4 shadow rounded-lg flex items-center'>
                   <span className='mr-2'><AiOutlineCheckCircle className='text-green-500' /></span>
                   <span className='flex-grow'>email.test1234@gmail.com</span>
                   <span><MdCancel className='text-red-400 hover:text-red-800 cursor-pointer' /></span>
                 </div>
-                <div className='w-3/4 m-2 p-4 shadow rounded-lg flex items-center'>
+                <div className='w-full md:w-1/2 m-2 p-4 shadow rounded-lg flex items-center'>
                   <span className='mr-2'><CiAirportSign1 className='text-yellow-400' /></span>
                   <span className='flex-grow'>jane.doe@gmail.com</span>
                   <span><MdCancel className='text-red-400 hover:text-red-800 cursor-pointer' /></span>
                 </div>
-                <div className='w-3/4 m-2 p-4 shadow rounded-lg flex items-center'>
+                <div className='w-full md:w-1/2 m-2 p-4 shadow rounded-lg flex items-center'>
                   <span className='mr-2'><CiAirportSign1 className='text-yellow-400' /></span>
                   <span className='flex-grow'>charlie.brown@gmail.com</span>
                   <span><MdCancel className='text-red-400 hover:text-red-800 cursor-pointer' /></span>
@@ -109,20 +136,26 @@ function App() {
             </div>
           </div>
           : navState === 'Matches' ?
-            <div className="mt-12 min-h-1/4 w-3/4 md:w-1/2 rounded-lg xl:w-1/2 flex shadow-xl  justify-center flex-row">
-              <div className='w-full flex flex-row'>
-                <div className='w-1/2 flex justify-start items-center flex-col'> Liked
-                  <div className='flex  flex-wrap text-center'>{likedNames.map((name) =>
-                    <span className='p-4 shadow m-2'> {name} </span>
-                  )}</div>
-                </div>
-                <div className='w-1/2 flex flex-col justify-start items-center'> Disliked
-                  <div className='flex  flex-wrap text-center'>{disLikedNames.map((name) =>
-                    <span className='p-4 m-2 shadow'> {name} </span>
-                  )}</div>
+            <div className="mt-12 min-h-1/4 w-3/4 md:w-1/2 rounded-lg xl:w-1/2 flex shadow-xl  justify-center  flex-col sm:flex-row">
+              <div className='md:w-1/2 flex justify-start items-center flex-col'> <div className='flex items-center'><SlLike className='mr-2' /> Liked</div>
 
-                </div>
+                <div className='flex  flex-wrap text-center justify-center'>{likedGirlNames.map((name) =>
+                  <span className='p-4 m-2 rounded-lg shadow-sm bg-pink-50 min-w-fit'> {name} </span>
+                )}</div>
+                <div className='flex  flex-wrap text-center'> {likedBoyNames.map((name) =>
+                  <span className='p-4 m-2 rounded-lg shadow-sm bg-blue-50 min-w-fit'> {name} </span>
+                )}</div>
               </div>
+              <div className='md:w-1/2 flex justify-start items-center flex-col'> <div className='flex items-center'><SlDislike className='mr-2' /> Dislike</div>
+
+                <div className='flex  flex-wrap text-center'> {disLikedGirlNames.map((name) =>
+                  <span className='p-4 m-2 rounded-lg shadow-sm bg-pink-50 min-w-fit'> {name} </span>
+                )}</div>
+                <div className='flex  flex-wrap text-center'> {disLikedBoyNames.map((name) =>
+                  <span className='p-4 m-2 rounded-lg shadow-sm bg-blue-50 min-w-fit'> {name} </span>
+                )}</div>
+              </div>
+
             </div>
             :
             <></>
