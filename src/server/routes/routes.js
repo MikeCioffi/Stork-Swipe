b9db27c8848ace70852137475d3f29a5270620c0
Mike Cioffi <mike.cioffi95@gmail.com>
2022-11-30 11:54:36 -0600

    cors fix

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -15,6 +15,12 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 module.exports = router;
 
+router.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 //Post Method to create new NAME in DB
 router.post('/name/post', jsonParser, async (req, res) => {
     const listData = new Model.nameData({
