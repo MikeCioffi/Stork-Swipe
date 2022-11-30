76a0004bb04c0facb7816d5977af783b5f1551fc
Mike Cioffi <mike.cioffi95@gmail.com>
2022-11-30 11:56:01 -0600

    cors fix

--- src/server/index.js
+++ src/server/index.js
@@ -33,6 +33,11 @@ const corsOptions = {
     optionSuccessStatus: 200
 }
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(cors(corsOptions));
 app.use('/api', routes)
