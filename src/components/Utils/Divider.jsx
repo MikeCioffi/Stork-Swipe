a3e77cc885a861fb136116d9970706f46b95af46
Mike Cioffi <mike.cioffi95@gmail.com>
2024-05-26 11:44:36 -0500

    all files added

---
+++ src/components/Utils/Divider.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Divider.css'; // Ensure the CSS file is correctly imported
+
+const Divider = () => (
+    <div className="divider"></div>
+);
+
+export default Divider;
