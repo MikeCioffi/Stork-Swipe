cbb86bce6061cac3bb84c085ca995f5afffa2c4d
Mike Cioffi <mike.cioffi95@gmail.com>
2024-07-08 12:38:16 -0500

    navbar css fixes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({ navState, setNavState, userData, resetUser }) => {
     };
 
     return (
-        <nav className='relative flex items-center w-full text-md md:text-3xl lg:text-4xl p-6 bg-white shadow-md h-20 md:h-24'>
+        <nav className='relative flex items-center w-full text-md md:text-3xl lg:text-4xl p-6 bg-white border-b border-gray-200 h-20 md:h-24'>
             <div className='flex justify-start md:justify-center items-center w-full h-full'>
                 <button
                     className='md:hidden p-4'
@@ -21,10 +21,10 @@ const Navbar = ({ navState, setNavState, userData, resetUser }) => {
                 >
                     <GiHamburgerMenu />
                 </button>
-                <div className={`fixed inset-0 top-20 bg-white shadow-md z-10 md:static md:shadow-none md:flex md:items-center md:space-x-6 ${isMobileMenuOpen ? 'flex flex-col items-center space-y-6' : 'hidden'}`}>
+                <div className={`fixed inset-0 top-20 bg-white z-10 text-center md:static md:flex md:items-center md:space-x-6 ${isMobileMenuOpen ? 'flex flex-col items-start space-y-6' : 'hidden'}`}>
                     <div
                         onClick={() => handleNavClick('Names')}
-                        className={`${navState === 'Names' ? 'bg-gray-200 text-primary' : 'text-gray-600'} flex items-center w-full md:w-auto p-4 cursor-pointer transition-all duration-300 ease-in-out rounded-lg hover:bg-gray-100 hover:text-primary`}
+                        className={`${navState === 'Names' ? 'text-primary' : 'text-gray-600'} flex items-center w-5/6  p-6 md:w-full h-16 cursor-pointer transition-all duration-300 ease-in-out rounded-lg hover:bg-gray-100 hover:text-primary`}
                     >
                         <FaBabyCarriage />
                         <span className='ml-2'>
@@ -34,7 +34,7 @@ const Navbar = ({ navState, setNavState, userData, resetUser }) => {
 
                     <div
                         onClick={() => handleNavClick('Partner')}
-                        className={`${navState === 'Partner' ? 'bg-gray-200 text-primary' : 'text-gray-600'} flex items-center w-full md:w-auto p-4 cursor-pointer transition-all duration-300 ease-in-out rounded-lg hover:bg-gray-100 hover:text-primary`}
+                        className={`${navState === 'Partner' ? 'text-primary' : 'text-gray-600'} flex items-center w-5/6  p-6 md:w-full h-16 cursor-pointer transition-all duration-300 ease-in-out rounded-lg hover:bg-gray-100 hover:text-primary`}
                     >
                         <BsFillPersonFill />
                         <span className='ml-2'>
@@ -44,7 +44,7 @@ const Navbar = ({ navState, setNavState, userData, resetUser }) => {
 
                     <div
                         onClick={() => handleNavClick('Matches')}
-                        className={`${navState === 'Matches' ? 'bg-gray-200 text-primary' : 'text-gray-600'} flex items-center w-full md:w-auto p-4 cursor-pointer transition-all duration-300 ease-in-out rounded-lg hover:bg-gray-100 hover:text-primary`}
+                        className={`${navState === 'Matches' ? 'text-primary' : 'text-gray-600'} flex items-center w-5/6  p-6 md:w-full h-16 cursor-pointer transition-all duration-300 ease-in-out rounded-lg hover:bg-gray-100 hover:text-primary`}
                     >
                         <RiCheckboxMultipleLine />
                         <span className='ml-2'>
@@ -55,10 +55,10 @@ const Navbar = ({ navState, setNavState, userData, resetUser }) => {
                 {userData.email.length > 0 &&
                     <div
                         onClick={resetUser}
-                        className='flex items-center p-4 cursor-pointer rounded-lg hover:bg-gray-100 transition-all duration-300 ease-in-out h-full absolute right-0'
+                        className='flex items-center h-16 p-4 cursor-pointer rounded-lg hover:bg-gray-100 transition-all duration-300 ease-in-out absolute right-2'
                     >
-                        <img src={userData.image_url} referrerPolicy="no-referrer" alt="user's profile" className='rounded-full h-8 w-8 sm:h-12 sm:w-12 md:h-16 md:w-16' />
-                        <span className='ml-2 text-md md:text-xl lg:text-2xl'>Sign Out</span>
+                        <img src={userData.image_url} referrerPolicy="no-referrer" alt="user's profile" className='rounded-full h-8 w-8' />
+                        <span className='ml-2 text-primary text-md md:text-xl lg:text-2xl'>Sign Out</span>
                     </div>
                 }
             </div>
