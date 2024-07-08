969540b0d0962577e4530a2345cf252bc287bc12
Mike Cioffi <mike.cioffi95@gmail.com>
2024-07-08 11:44:58 -0500

    fixed matches page, added spinner

--- src/components/NameSection/NameSection.jsx
+++ src/components/NameSection/NameSection.jsx
@@ -3,48 +3,64 @@ import NameCard from '../NameCard/NameCard';
 import './NameSection.css'; // Import the CSS file for animations and styles
 
 const NamesSection = ({ title, data = [], friendsData, actionType, toggleActionStatus, userData }) => {
-    const [isLoaded] = useState(data.length > 0);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [matches, setMatches] = useState([]);
+    const [nonMatches, setNonMatches] = useState([]);
 
-    const boysData = data.filter(item => item.gender === 'boy');
-    const girlsData = data.filter(item => item.gender === 'girl');
+    console.log("friendData:", friendsData);
+    console.log("data:", data);
+
+    // This will run synchronously and update state immediately
+    if (data.length > 0 && friendsData && friendsData.length > 0 && !isLoaded) {
+        const matchResults = data.filter(item =>
+            friendsData.some(friendGroup =>
+                friendGroup.data.some(friendItem => friendItem === item.name)
+            )
+        );
+        const nonMatchResults = data.filter(item =>
+            !friendsData.some(friendGroup =>
+                friendGroup.data.some(friendItem => friendItem === item.name)
+            )
+        );
+        setMatches(matchResults);
+        setNonMatches(nonMatchResults);
+        setIsLoaded(true);
+    }
+
+    const renderSection = (sectionTitle, sectionData) => (
+        <div className='w-full'>
+            <div className='text-2xl font-semibold p-3'>{sectionTitle}</div>
+            <div className='flex h-auto justify-start w-full items-center flex-row flex-wrap'>
+                {sectionData.map((item) => (
+                    <div key={item.nameid} className={`w-full md:w-1/4 xl:w-1/6 p-2`}>
+                        <NameCard
+                            namekey={item.nameid}
+                            item={item}
+                            actionType={actionType}
+                            friendActions={friendsData}
+                            toggleActionStatus={toggleActionStatus}
+                            userData={userData}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 
     return (
         <div className={`min-h-1/4 w-full items-center m-auto p-5 rounded-xl shadow-lg bg-white mb-10 ${isLoaded ? 'animate-fade-in' : ''}`}>
             <div className='text-3xl font-bold w-full text-center p-5'>{title}</div>
-            <div className='w-full'>
-                <div className='text-2xl font-semibold p-3'>Boys</div>
-                <div className='flex h-auto justify-start w-full items-center flex-row flex-wrap'>
-                    {boysData.map((item, index) => (
-                        <div key={item.nameid} className={`w-full md:w-1/4 xl:w-1/6 p-2 '}`}>
-                            <NameCard
-                                namekey={item.nameid}
-                                item={item}
-                                actionType={actionType}
-                                friendActions={friendsData}
-                                toggleActionStatus={toggleActionStatus}
-                                userData={userData}
-                            />
-                        </div>
-                    ))}
+            {isLoaded ? (
+                <>
+                    {matches.length > 0 && renderSection('Matches', matches)}
+                    {nonMatches.length > 0 && renderSection('Non Matches', nonMatches)}
+                </>
+            ) : (
+                <div className='text-center'>
+                    <div className='spinner'></div> {/* Replace with your spinner component or styling */}
+                    <p>Loading...</p>
                 </div>
-            </div>
-            <div className='w-full mt-5'>
-                <div className='text-2xl font-semibold p-3'>Girls</div>
-                <div className='flex h-auto justify-start w-full items-center flex-row flex-wrap'>
-                    {girlsData.map((item, index) => (
-                        <div key={item.nameid} className={`w-full md:w-1/4 xl:w-1/6 p-2 '}`}>
-                            <NameCard
-                                namekey={item.nameid}
-                                item={item}
-                                actionType={actionType}
-                                friendActions={friendsData}
-                                toggleActionStatus={toggleActionStatus}
-                                userData={userData}
-                            />
-                        </div>
-                    ))}
-                </div>
-            </div>
+            )}
         </div>
     );
 };
--- src/components/Pages/MatchesPage.jsx
+++ src/components/Pages/MatchesPage.jsx
@@ -1,35 +1,59 @@
 import React from 'react';
 import NamesSection from '../NameSection/NameSection';
-import { SlLike, SlDislike } from 'react-icons/sl'
+import { SlLike, SlDislike } from 'react-icons/sl';
+import Spinner from '../Utils/Spinner'; // Import the spinner component
 
 
 const MatchesPage = ({
     likedData, disLikedData, friendLikes, friendDisLikes, toggleActionStatus, userData
-}) => (
-    <div className=" w-11/12 flex flex-col justify-around items-center bg-white">
+}) => {
+    const isDataReady = likedData.length > 0 && disLikedData.length > 0 && friendLikes.length > 0 && friendDisLikes.length > 0;
 
-        <NamesSection
-            title="Liked"
-            data={likedData}
-            friendsData={friendLikes}
-            toggleActionStatus={toggleActionStatus}
-            actionType="dislike"
-            icon={<SlLike className='mr-2 text-green-500' />}
-            userData={userData}
-        />
+    const getSortedData = (data, friendData) => {
+        const matches = data.filter(item => friendData.some(friendItem => friendItem.nameid === item.nameid));
+        const others = data.filter(item => !friendData.some(friendItem => friendItem.nameid === item.nameid));
+        return [...matches, ...others];
+    };
 
-        <NamesSection
-            title="Disliked"
-            data={disLikedData}
-            friendsData={friendDisLikes}
-            toggleActionStatus={toggleActionStatus}
-            actionType="like"
-            icon={<SlDislike className='mr-2 text-red-500' />}
-            userData={userData}
-        />
+    let sortedLikedData = [];
+    let sortedDisLikedData = [];
 
+    if (isDataReady) {
+        sortedLikedData = getSortedData(likedData, friendLikes);
+        sortedDisLikedData = getSortedData(disLikedData, friendDisLikes);
+    }
 
-    </div>
-);
+    if (!isDataReady) {
+        return (
+            <div className="text-center">
+                <Spinner /> {/* Use the spinner component */}
+
+            </div>
+        );
+    }
+
+    return (
+        <div className="w-11/12 flex flex-col justify-around items-center bg-white">
+            <NamesSection
+                title="Liked"
+                data={sortedLikedData}
+                friendsData={friendLikes}
+                toggleActionStatus={toggleActionStatus}
+                actionType="dislike"
+                icon={<SlLike className='mr-2 text-green-500' />}
+                userData={userData}
+            />
+            <NamesSection
+                title="Disliked"
+                data={sortedDisLikedData}
+                friendsData={friendDisLikes}
+                toggleActionStatus={toggleActionStatus}
+                actionType="like"
+                icon={<SlDislike className='mr-2 text-red-500' />}
+                userData={userData}
+            />
+        </div>
+    );
+};
 
 export default MatchesPage;
